ef7943a21e19d9a27f3d4de12e056f1c59301cea
lisethmdc <lismc1992@gmail.com>
2022-03-06 15:44:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/_main.scss";
+
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Wallet from "./pages/Wallet/Wallet";
+import ModeContext from "./context/ModeContext";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ModeContext>
+        <Router>
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="wallet" element={<Wallet />}></Route>
+          </Routes>
+        </Router>
+      </ModeContext>
     </div>
   );
 }
