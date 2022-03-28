d3b6970428fa0e960091e6fd5c6023511e90723e
lisethmdc <lismc1992@gmail.com>
2022-03-28 22:23:00 +0200

    adding Login and styles

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LoginReducer from "../../components/Login/Login";
+import "./styles.scss";
+import imageHome from "../../assets/images/Standing.png";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="homeContainer">
+        <LoginReducer />
+      </div>
+      <div className="imageContainer">
+        <img className="img-home" src={imageHome} alt="ilustración home" />
+      </div>
+    </>
+  );
+}
